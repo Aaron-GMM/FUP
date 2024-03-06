f3e7719025a1619f567d6b1cc8a7044badf41dd7
Aaron <aarongibran13@gmail.com>
2024-03-06 10:41:43 -0300

    Simulador de Caixa eletronica concluido com erro

--- Atividades/simuCaixa.c
+++ Atividades/simuCaixa.c
@@ -6,33 +6,67 @@ int main()
 {
     int conta[10] = {0}, saldo[10] = {0};
     int op = 0;
+    int saque = 0;
+    // #define e true;
     bool e = true;
     for (int i = 0; i < 10; i++)
     {
-        printf("Digite o numero da conta");
+        printf("\nDigite o numero da conta");
         scanf("%d", &conta[i]);
+        e = true;
         while (e == true)
         {
             if (conta != NULL)
             {
-                printf("Conta %d \n Escolha a operacao:\n\t1-Saldo\n\t2-Depositar\n\t3-Saquen\n\t4-digite pra sair", conta[i]);
+                printf("\nConta %d \n Escolha a operacao:\n\t1-Saldo\n\t2-Depositar\n\t3-Saque\n\t4-Digite pra sair", conta[i]);
                 scanf("%d", &op);
                 switch (op)
                 {
-                case 1:
 
-                    printf("Conta N:%d Saldo eh:%d", conta[i], saldo);
+                case 1:
+                    printf("\nStatus do  saldo da conta  N:%d Saldo: %d", conta[i], saldo[i]);
                     break;
+
                 case 2:
+                    printf("\nConta N:%d\nDigite o valor do deposito:", conta[i]);
+                    scanf("%d", &saldo[i]);
+                    if (saldo[i] != NULL)
+                    {
+                        printf("\nDeposito efetuado com sucesso!");
+                        printf("\nStatus do  saldo da conta  N:%d Saldo: %d", conta[i], saldo[i]);
+                    }
+                    else
+                    {
+                        printf("\n Erro no deposito");
+                    }
                     break;
 
                 case 3:
+                    printf("\nConta N:%d\nDigite o valor que deseja sacar", conta[i]);
+                    scanf("%d", &saque);
+                    if (saldo[i] >= saque)
+                    {
+                        saldo[i] -= saque;
+                        printf("\n Saque efetuado com sucesso!");
+                        printf("\nStatus do  saldo da conta  N:%d Saldo:%d", conta[i], saldo[i]);
+                    }
+                    else
+                    {
+                        printf("\nSaldo Indisponivel\n");
+                    }
+                    break;
 
                 default:
+                    e = false;
                     break;
                 }
             }
         }
+    }
+    for (int x = 0; x < 10; x++)
+    {
+        printf("\nConta: %d \n", conta[x]);
+        printf("\nSaldo %d\n", saldo[x]);
     }
 
     return 0;
