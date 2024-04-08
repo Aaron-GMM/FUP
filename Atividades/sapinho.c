4cd88b55a6bc63021348135d2c268c1ca99e9133
Aaron <aarongibran13@gmail.com>
2024-04-07 23:02:01 -0300

    Nova atividade de fup

--- Atividades/sapinho.c
+++ Atividades/sapinho.c
@@ -1,4 +1,21 @@
 #include <stdio.h>
-int main(){
-    
+
+int main() {
+    int p, s, e;
+    scanf("%d", &p);
+    scanf("%d", &s);
+    scanf("%d", &e);
+
+    int posicao = 0;
+    while (posicao < p) {
+        if (posicao + s >= p) {
+            printf("%d saiu\n", posicao);
+            break;
+        } else {
+            printf("%d %d\n", posicao, posicao + s);
+            posicao += (s - e);
+        }
+    }
+
+    return 0;
 }
